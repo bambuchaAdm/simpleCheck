b98724545405949151b01844824fbb25afac1dce
bambucha <bambucha14@gmail.com>
2011-12-14 16:26:22 +0100

    Dodanie klasy Tester

---
+++ src/pl/krakow/vlo/simplechecker/Tester.java
@@ -0,0 +1,16 @@
+
+package pl.krakow.vlo.simplechecker;
+
+public class Tester
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Brak implementacji");
+	}
+
+}
